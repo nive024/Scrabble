618c3fb90b42519efc98fa9fba96261dabe873fe
Nivetha <nive0204@gmail.com>
2022-10-25 19:45:05 -0400

    changed variable type from Character and Integer to char and int and added constructor chaining

--- src/Letters.java
+++ src/Letters.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Letters {
     private static final Map<Character, Integer> Alphabet= new HashMap<>();
 
-    private Character Letter;
-    private Integer Value;
+    private char letter;
+    private int value;
 
     public Letters(){
         Alphabet.put('A', 1);
@@ -35,42 +35,16 @@ public class Letters {
         Alphabet.put('Z', 10);
     }
 
-    public Letters(Character letter){
-        Alphabet.put('A', 1);
-        Alphabet.put('B', 3);
-        Alphabet.put('C', 3);
-        Alphabet.put('D', 2);
-        Alphabet.put('E', 1);
-        Alphabet.put('F', 2);
-        Alphabet.put('G', 2);
-        Alphabet.put('H', 4);
-        Alphabet.put('I', 1);
-        Alphabet.put('J', 8);
-        Alphabet.put('K', 5);
-        Alphabet.put('L', 1);
-        Alphabet.put('M', 3);
-        Alphabet.put('N', 1);
-        Alphabet.put('O', 1);
-        Alphabet.put('P', 3);
-        Alphabet.put('Q', 10);
-        Alphabet.put('R', 1);
-        Alphabet.put('S', 1);
-        Alphabet.put('T', 1);
-        Alphabet.put('U', 1);
-        Alphabet.put('V', 4);
-        Alphabet.put('W', 4);
-        Alphabet.put('X', 8);
-        Alphabet.put('Y', 4);
-        Alphabet.put('Z', 10);
-
-        this.Letter = letter;
-        Value = getPointValue(letter);
+    public Letters(char letter){
+        super();
+        this.letter = letter;
+        value = getPointValue(letter);
     }
 
-    public Character getLetter(){
-        return Letter;
+    public char getLetter(){
+        return letter;
     }
-    public int getPointValue(Character c){ //return the point value associated with the letter
+    public static int getPointValue(Character c){ //return the point value associated with the letter
         return Alphabet.get(c);
     }
 
