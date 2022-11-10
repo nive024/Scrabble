0b83a87ff7ed6024312469705cf10472ff36e850
nicolelim <nicolelim@cmail.carleton.ca>
2022-11-09 19:26:02 -0500

    Added PlayerTest file to test methods in Player class

---
+++ src/PlayerTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+    @Test
+    public void testGetPlayerName(){
+        Player p = new Player("Homer");
+        assertEquals(p.getName(), "Homer");
+    }
+
+    @Test
+    public void testSetAndGetPlayerScore(){
+        Player p = new Player("Homer");
+        p.setScore(30);
+        assertEquals(p.getScore(), 30);
+    }
+
+    @Test
+    public void testSetLetters(){
+        Player p = new Player("Homer");
+        ArrayList<Letters> l = new ArrayList<>();
+
+        p.setLetters(l);
+    }
+
+}
