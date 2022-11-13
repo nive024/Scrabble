740691a2a36c4af6184cdc317f944c0b1022de64
Nivetha <nive0204@gmail.com>
2022-11-13 08:27:10 -0500

    removed ScrabbleEvent class

--- src/ScrabbleEvent.java
+++
@@ -1,9 +0,0 @@
-import java.util.EventObject;
-
-public class ScrabbleEvent extends EventObject {
-
-    public ScrabbleEvent (GameBoard model, String word, String Place) {
-        super(model);
-    }
-
-}
